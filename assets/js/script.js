a8a1ecbaebd1af3518cbf5dcdaab69f121cb0337
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-08-23 16:53:04 -0700

    add input validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,16 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var taskFormHandler = function (event) {
     event.preventDefault();
+
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset();
 
     // package up data as an object
     var taskDataObj = {
